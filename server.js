b839e0c8ef53035b72a6823684ac9da7e4ee55f8
deanusama <deanusama@gmail.com>
2023-05-29 18:20:54 +0500

    changes

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: "https://eti-frontend.vercel.app"
 }))
 
 app.use("/uploads", express.static('uploads'))
